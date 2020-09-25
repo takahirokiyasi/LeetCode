03601062d09ab37c8b064dc3c4cbaded8ab12524
takahirokiyasi <okiyasi@gmail.com>
2020-09-25 23:51:27 +0900

    add 6

---
+++ problems/6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        ans = ''
+        if numRows != 1:
+            one_cycle_num = (numRows - 2) * 2 + 2
+        else:
+            one_cycle_num = 1
+        s_list = list(s)
+        length = len(s_list)
+        zigzag = math.ceil(length / one_cycle_num)
+        for j in range((one_cycle_num // 2) + 1):
+            for i in range(zigzag):
+                index = j + i * one_cycle_num
+                try:
+                    ans += s_list[index]
+                except IndexError:
+                    pass
+                second_index = -j + (i + 1) * one_cycle_num
+                if j == 0 or index == second_index:
+                    continue
+                try:
+                    ans += s_list[second_index]
+                except IndexError:
+                    pass
+        return ans
