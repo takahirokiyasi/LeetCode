6323af0a3362e485d273a95d135742ed21c65739
takahirokiyasi <okiyasi@gmail.com>
2021-02-24 23:54:24 +0900

    add url

--- leetcode/daily_problems/Feb-24-2.py
+++ leetcode/daily_problems/Feb-24-2.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3651/
 S = input()
 crr, stack = 0, []
 for pos in range(len(S)):
