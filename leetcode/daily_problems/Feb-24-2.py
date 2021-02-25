186ec05c3e9b6b75e37f29f3900fc8c47315b677
takahirokiyasi <okiyasi@gmail.com>
2021-02-25 13:07:26 +0900

    try again

--- leetcode/daily_problems/Feb-24-2.py
+++ leetcode/daily_problems/Feb-24-2.py
@@ -1,15 +1,14 @@
 # https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3651/
 S = input()
-crr, stack = 0, []
-for pos in range(len(S)):
-    if S[pos] == '(':
+stack = []
+tmp = 0
+for i, s in enumerate(S):
+    if s == '(':
         stack.append('(')
     else:
-        # stackの一番後ろに数字じゃなくて(が出てくるまでpopする
         while stack[-1] != '(':
-            crr += stack[-1]
+            tmp += stack[-1]
             stack.pop()
-        # crrが0だったら()があったということなので1、それ以外だと（A）なので二倍
-        stack[-1] = crr * 2 if crr else 1
-        crr = 0
+        stack[-1] = tmp * 2 if tmp else 1
+        tmp = 0
 print(sum(stack))
