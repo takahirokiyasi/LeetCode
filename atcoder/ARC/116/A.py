5875086e3c1c282a515c43478c6c9c7edfd1d395
takahirokiyasi <okiyasi@gmail.com>
2021-03-29 21:41:41 +0900

    fix ans

--- atcoder/ARC/116/A.py
+++ atcoder/ARC/116/A.py
@@ -1,28 +1,9 @@
-def divisor(n):
-    i = 1
-    table = []
-    odd = 0
-    even = 0
-    while i * i <= n:
-        if n % i == 0:
-            if i % 2 == 0:
-                even += 1
-            else:
-                odd += 1
-            table.append(n//i)
-            if (n // i) % 2 == 0:
-                even += 1
-            else:
-                odd += 1
-        i += 1
-    if odd == even:
-        return 'Same'
-    elif odd > even:
-        return 'Odd'
-    else:
-        return 'Even'
-
-
 T = int(input())
 for i in range(T):
-    print(divisor(int(input())))
+    tmp = int(input())
+    if tmp % 4 == 0:
+        print('Even')
+    elif tmp % 2 == 0:
+        print('Same')
+    else:
+        print('Odd')
