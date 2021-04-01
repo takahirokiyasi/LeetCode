2bb5800d459930d81b6c684384e65407d88ac5f5
takahirokiyasi <okiyasi@gmail.com>
2021-04-01 22:42:36 +0900

    add comment

--- typical_problems/bit-full-search/abc-197-C.py
+++ typical_problems/bit-full-search/abc-197-C.py
@@ -1,14 +1,19 @@
 # https://atcoder.jp/contests/abc197/tasks/abc197_c
 
+# bit全探索
+# https://qiita.com/gogotealove/items/11f9e83218926211083a
+
 N = int(input())
 A = list(map(int, input().split()))
 ans = float('inf')
 
+# Aの各間をORにするかXORするかなのでN-1
 for i in range(2**(N-1)):
     xored = 0
     ored = 0
     for j in range(N-1):
         ored |= A[j]
+        # 順に一つずつビットシフトしていき1の時はそれが選ばれているとする
         if (i >> j) & 1:
             xored ^= ored
             ored = 0
